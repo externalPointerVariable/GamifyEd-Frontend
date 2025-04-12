3d0ca1923d116bd9c61012a6ed50d7379248c976
externalPointerVariable <vicky986847@gmail.com>
2025-04-12 15:58:37 +0000

    refactor: clean up store configuration and remove unused imports

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,31 +1,21 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {setAchievements,
-    setExperience,
-    setLevel,
-    setMissions,
-    setAuthError,
-    setAuthLoading,
-    setClassroomError,
-    setClassroomLoading,
-    setCreatedClassrooms,
-    setCurrentClassroom,
-    setJoinedClassrooms,
-    setPracticeTests,
-    setTestQuizzes,
-    setCurrentTest,
-    setTestsError,
-    setTestsLoading,
-    clearUserProfile,
-    clearUser,
-    setTheme,
-    setUserError,
-    setUserLoading,
-    setUserProfile,
-    setUser,} from '../features/index.js'; 
+import { configureStore } from '@reduxjs/toolkit';
+
+import authReducer from '../features/authSlice.js';
+import classroomsReducer from '../features/classroomsSlice.js';
+import testsReducer from '../features/testsSlice.js';
+import gamificationReducer from '../features/gamificationSlice.js';
+import userReducer from '../features/userSlice.js';
+import uiReducer from '../features/uiSlice.js';
 
 const store = configureStore({
-    reducer: {
-        // Add your reducers here
-    },
+  reducer: {
+    auth: authReducer,
+    classrooms: classroomsReducer,
+    tests: testsReducer,
+    gamification: gamificationReducer,
+    user: userReducer,
+    ui: uiReducer,
+  },
 });
+
 export default store;

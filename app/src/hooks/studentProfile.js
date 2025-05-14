0fe75346d0e8674383572db18f3b073357595f52
externalPointerVariable <vicky986847@gmail.com>
2025-05-14 05:52:27 +0000

    feat: Implement getUserProfile and updateUserProfile functions for user profile management

---
+++ app/src/hooks/studentProfile.js
@@ -0,0 +1,8 @@
+import config from "../config/config";
+import {useSelector} from "react-redux";
+
+const getStudentProfile = async () => {};
+
+const updateStudentProfile = async () => {};
+
+export {getStudentProfile, updateStudentProfile};
